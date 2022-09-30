5e4e7dcb8b953d4883138423b3a9dcede0579f20
nasalcavity <107585440+douxyani@users.noreply.github.com>
2022-09-30 22:00:22 +0100

    update settings
    enable and disable individual services for notis

--- models/tokenDB.js
+++ models/tokenDB.js
@@ -2,5 +2,21 @@ const { model, Schema } = require("mongoose");
 
 module.exports = model('token', new Schema({
     id: String,
-    pushToken: String
+    pushToken: String,
+    chipotle: {
+        type: Boolean,
+        default: true
+    },
+    fooji: {
+        type: Boolean,
+        default: true
+    },
+    promo: {
+        type: Boolean,
+        default: true
+    },
+    deal: {
+        type: Boolean,
+        default: true
+    }
 }))
--- routes.js
+++ routes.js
@@ -17,13 +17,41 @@ router.post("/tokens", async (req, res) => {
   res.send({ status: 'ok' });
 });
 
+router.post('/tokens/notis/:id', async (req, res) => {
+  try {
+    await tokenDB.findOneAndUpdate({ 
+      id: req.params.id
+    }, {
+      ...req.body
+    });
+    res.send({ status: 'ok' });
+  } catch {
+    res.status(404);
+    res.send({ error: "User doesn't exist!" });
+  }
+})
+
+
+router.get('/tokens/notis/:id', async (req, res) => {
+  try {
+    const data = await tokenDB.findOne({
+      id: req.params.id
+    })
+
+    res.send({ status: 'ok', data: JSON.stringify(data) });
+  } catch {
+    res.status(404);
+    res.send({ error: "User doesn't exist!" });
+  }
+})
+
 router.delete("/tokens/:id", async (req, res) => {
   try {
     await tokenDB.findOneAndDelete({ id: req.params.id });
     res.status(204).send({ status: 'ok' });
   } catch {
     res.status(404);
-    res.send({ error: "Post doesn't exist!" });
+    res.send({ error: "User doesn't exist!" });
   }
 });
 
